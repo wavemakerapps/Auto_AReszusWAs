78b1f0117b1b15067aea93c4bb4e27e278fba20d
ZMlWxmArSN <automate1@wavemaker.com>
2017-10-09 21:22:46 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_AReszusWAs",
+  "homePage" : "Main",
+  "name" : "Auto_AReszusWAs",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
